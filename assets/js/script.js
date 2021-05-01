eb19da9743f5bbba20d4f36c31445d6ac9335d42
Taylor Smith <tbsmith0205@gmail.com>
2021-05-01 12:39:03 -0600

    calling API's in script.js

---
+++ assets/js/script.js
@@ -0,0 +1,69 @@
+var cityName = "Denver";
+function getApi() {
+  console.log("hit");
+  // fetch request gets a list of all the repos for the node.js organization
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/forecast?q=" +
+    cityName +
+    "&appid=e967a440a143ceff5c2a57cc190f7809";
+
+  // Promises - returns us a Promise object
+
+  // synchronous vs asynchronous code
+
+  // JS by default is synchronous
+
+  fetch(requestUrl)
+    .then(function (response) {
+      //   console.log(response);
+      // the json() method of the body mix in takes a response stream and reads it to completion. It returns a promise which fesolves with a Javascript object that is the result of parsing the body text as JSON
+
+      // Note that despite the method being named json(), the result is not JSON but is instead the result of taking JSON as input and parsing it to produce a Javascript object
+
+      // the Fetch API allows you to asynchronously request for a resource. Use the fetch() method to return a promise that resolves into a Response object e.g., text() or json().
+
+      return response.json();
+    })
+    .then(function (data) {
+      var longitude = data.city.coord.lon;
+      var latitude = data.city.coord.lat;
+      console.log(longitude);
+      console.log(latitude);
+
+      console.log(data); // this is the data that was returned
+      //Loop over the data to generate a table, each table row will have a link to the repo url
+      var oneCall =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        latitude +
+        "&lon=" +
+        longitude +
+        "&exclude=hourly,minutely&appid=e967a440a143ceff5c2a57cc190f7809";
+      fetch(oneCall)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+        }); // anything we are doing with the data will be stored in curly brackets
+      // temp is in K, take temp and subtract 273.15 (temp - 273.15) * 1.8 + 32
+      for (var i = 0; i < data.length; i++) {
+        // data.length represents the number of objects in the array (30)
+        // Creating elements, tablerow, tabledata, and anchor
+        var createTableRow = document.createElement("tr");
+        var tableData = document.createElement("td");
+        var link = document.createElement("a");
+
+        // Setting the text of link and the href of the link
+        link.textContent = data[i].html_url;
+        link.href = data[i].html_url;
+
+        // Appending the link to the tabledata and then appending the tabledata to the tablerow
+        // The tablerow then gets appended to the tablebody
+        tableData.appendChild(link);
+        createTableRow.appendChild(tableData);
+        tableBody.appendChild(createTableRow);
+      }
+    });
+}
+var fetchButton = document.getElementById("submit");
+fetchButton.addEventListener("click", getApi);
